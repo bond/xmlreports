ed36563208a07e32abf17692475e13263f8bdcc1
Daniel Bond <gitbits@danielbond.org>
2008-05-07 13:11:02 +0200

    - removed old output-files

--- src/output.h
+++
@@ -1,8 +0,0 @@
-#ifndef _OUTPUT_H
-#define _OUTPUT_H
-
-extern int   write_main_index();                    /* produce main HTML   */
-extern int   write_month_html();                    /* monthy HTML page    */
-extern FILE  *open_out_file(char *);                /* open output file    */
-
-#endif  /* _OUTPUT_H */
